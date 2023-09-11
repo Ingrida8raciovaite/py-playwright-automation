b753e56c154652ebcb00ca32cb359c2f6d18f7fe
Ingrida <ingrida.s.graciovaite@gmail.com>
2023-09-11 11:56:08 +0200

    added test git

--- tests/test_tc.py
+++ tests/test_tc.py
@@ -1,6 +1,5 @@
 class TestMyClass:
-
-    # повідомлення буде вивовдитися перед кожним тестом
+    # The message will be displayed before each test in the console
     # def setup(self):
     #     print("start")
 
@@ -28,6 +27,6 @@ class TestMyClass:
         assert desktop_app_auth.test_cases.check_test_exists(test_name)
         desktop_app_auth.test_cases.delete_test_by_name(test_name)
 
-    # повідомлення буде виводитися вкінці кожного тесту
+    # The message will be displayed before each test in the console
     # def teardown(self):
     #     print('Finish')
