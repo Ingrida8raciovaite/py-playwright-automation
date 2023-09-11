4ea47eb318604923466996544452ecf844b841cf
Ingrida <ingrida.s.graciovaite@gmail.com>
2023-09-11 12:05:38 +0200

    updated comments

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -2,7 +2,6 @@ import json
 
 
 def test_dashboard_data(desktop_app_auth):
-    # перетворюємо на string
     payload = json.dumps({"total": 0, "passed": 0, "failed": 0, "norun": 0})
     desktop_app_auth.intercept_requests('**/getstat*', payload)
     desktop_app_auth.refresh_dashboard()
