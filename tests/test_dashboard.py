2b72b9fcc4d68baaae4cca638049649916e558f1
Ingrida <ingrida.s.graciovaite@gmail.com>
2023-09-16 15:43:58 +0300

    added test for multiple roles

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -8,5 +8,17 @@ def test_dashboard_data(desktop_app_auth):
     desktop_app_auth.refresh_dashboard()
     print(payload)
     print(desktop_app_auth.get_total_tests_stats())
-    assert desktop_app_auth.get_total_tests_stats() == "0"
     desktop_app_auth.stop_intercept('**/getstat*')
+    assert desktop_app_auth.get_total_tests_stats() == "0"
+
+
+def test_multiple_roles(desktop_app_auth, desktop_app_bob, get_db):
+    alice = desktop_app_auth
+    bob = desktop_app_bob
+    before = alice.get_total_tests_stats()
+    bob.navigate_to('Create new test')
+    bob.create_test('test by bob', 'test')
+    alice.refresh_dashboard()
+    after = alice.get_total_tests_stats()
+    get_db.delete_test_case('test by bob')
+    assert int(before) + 1 == int(after)
